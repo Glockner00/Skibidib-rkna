efa77895d1b77cd879a2bd1e7e87a58979e130f4
Olle Håkansson <ollesplugg@gmail.com>
2023-02-02 09:58:28 +0100

    testar igen

--- labb1/updf3.1.py
+++ labb1/updf3.1.py
@@ -1,5 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linespace(0, 16, 161)
+x = np.linspace(0, 16, 161)
+x = 10 ** -x
+f1 = (np.exp(x)-1)/x
+
+max_index = np.argmax(np.abs(f1-1))
+min_index = np.argmin(np.abs(f1-1))
+
+plt.loglog(x,np.abs(f1-1))
+plt.plot(x[max_index], np.abs(f1-1)[max_index], 'ro', markersize=10)
+plt.plot(x[min_index], np.abs(f1-1)[min_index], 'bo', markersize=10)
+plt.show()
+print("Det största värdet blir :" , max_index)
+print("Det minsta värdet blir :" , min_index)
+
+#Vid små x så ökar felet. Det minsta felet fås när x ungefär är 1,48 * 10^-8
 

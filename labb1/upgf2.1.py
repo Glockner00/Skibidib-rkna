876281d0819e7bc046234221c7cb79dbec8bbbc0
Axel Glöckner <axel.glockner00@gmail.com>
2023-02-06 13:15:48 +0100

    fortsättning labb1

--- labb1/upgf2.1.py
+++ labb1/upgf2.1.py
@@ -4,6 +4,5 @@ while (x != 1):
     print(x)
     n += 1
     x = 1+(2 ** -n)
-
 print("t blir : ", (n-1))
 print("My blir : ",(1/2)*2 ** (-(n-1)))
---
+++ labb1/upgf3.2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+x = np.linspace(-2 -2, 100)
+n = 5 # number of terms in the Taylor series, max number of terms we could approximate. 
+
+def f(x):
+    return (np.exp(x)-1)/x
+
+def exp_taylor(x, n):
+    result = 0
+    for i in range(n):
+        result += x**i/np.math.factorial(i)
+    return result
+
+y_true = f(x)
+y_taylor = exp_taylor(x, n)  # Approximation using the Taylor series
+
+RT = np.abs(y_true-y_taylor)
+
+plt.plot(x, RT, label='RT')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('RT')
+plt.title(f'Truncation Error RT with {n} terms in Tylor series')
+plt.show()
+
+# Vi har plottat y = e^x - 1 / x med hjälp av en taylor serie och jämfört med tidigare uppgift. 
+# trunkeringsfelet (approximiationen - exakt värde)  = 8.41877679e-11 ? 

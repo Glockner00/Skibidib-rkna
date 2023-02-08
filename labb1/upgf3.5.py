69c4f7b9c9a808ebf1627ca0f651f7d4b03aebf1
Axel Glöckner <axel.glockner00@gmail.com>
2023-02-08 12:07:54 +0100

    3.5, 3.6

--- labb1/upgf3.5.py
+++ labb1/upgf3.5.py
@@ -7,13 +7,11 @@ x = 10 ** -x
 
 my = (1/2)*2 ** (-(52))
 
-
 f2 = (np.exp(x)-1)/ (np.log(np.exp(x)))
 f3 = (x/2)
-f4 = (my/np.abs(x))
+f4 = (my/2)
 
-
-plt.loglog(x, np.abs(f2-1), label='|f2- 1|')
+plt.loglog(x, np.abs(f2-1), label='|f2 - 1|')
 plt.loglog(x, (f3+f4), label='R_TOT')
 plt.legend()
 plt.xlabel('x')

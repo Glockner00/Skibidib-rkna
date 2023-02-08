9010085ea86582cf8bf60720a4cd0dcd3d0aa68a
Axel Glöckner <axel.glockner00@gmail.com>
2023-02-08 14:47:07 +0100

    bajs

--- labb1/code/upgf4.2.py
+++ labb1/code/upgf4.2.py
@@ -2,33 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-x = np.linspace(-10, 10, 1000)
+x1 = np.linspace(-10, 0, 500)
+x2 = np.linspace(0, 10, 500)
 
-def min_exp(x):
-    S = 0.0
-    if x.any()>=0.0:
-        for k in range(0,100):
-            Tk=x**k/math.factorial(k)
-            S=S+Tk
-    else:
-        for k in range(0, 100):
-            Tk = (-1)*k / (math.factorial(k) * x*k)
-    return S 
+def min_exp_pos(x2):
+    S=0.0
+    for k in range(0,100):
+        Tk=x2**k/math.factorial(k)
+        S=S+Tk
+    return S
 
-def f(x):
-    return np.exp(x)
+def min_exp_neg(x1):
+    S=0.0
+    for k in range(0,100):
+        Tk=x1**k/math.factorial(k)
+        S=S+Tk
+    return 1/S
 
-absError = np.abs(min_exp(x) - f(x))
-relError = np.abs(absError/f(x))
+def f_pos(x2):
+    return np.exp(x2)
+def f_neg(x1):
+    return np.exp(x1)
 
-plt.plot(x, absError, label='The Absolute Error')
+absError = np.abs(min_exp_neg(x1) - f_neg(x1))
+relError = np.abs(absError/f_neg(x1))
+
+absError1 = np.abs(min_exp_pos(x2) - f_pos(x2))
+relError1 = np.abs(absError/f_pos(x2))
+
+plt.plot(x1, absError, label='The Absolute Error')
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('Abs_Error')
 plt.title('')
 plt.show()
 
-plt.plot(x, relError, label='The Relative Error')
+plt.plot(x1, relError, label='The Relative Error')
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('Rel_Error')
